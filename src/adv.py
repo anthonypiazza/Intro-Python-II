2bb57106a862d5162bb63504935677a9b3043111
anthonypiazza <piazza.anthony.n@gmail.com>
2019-10-31 16:03:35 -0500

    Added Item list into rooms, terminal messages display individual items

--- src/adv.py
+++ src/adv.py
@@ -4,23 +4,53 @@ from item import Item
 
 # Declare all the rooms
 
+cookies = Item("Cookies", "Feeling Hungry?") 
+mojito = Item("Mojito", "Are you thirsty?")
+pizza = Item("Pizza", "Its dinnertime!")
+hacksaw = Item("Hacksaw", "This will cut through doors")
+torch = Item("Torch", "Lets roast these marshmallows")
+shoes = Item("Shoes", "Get some exercise ya goof")
+pumpkin = Item("Pumpkin", "Happy Halloween")
+knife = Item("Knife", "Be careful its sharp")
+toilet = Item("Toilet", "Everyones gotta go eventually")
+pillow = Item("Pillow", "You've been walking awhile, take a nap.")
+pen = Item("Pen", "For writing")
+paper = Item("Paper", "Write down clues")
+pony = Item("Pony", "Lets get out of here")
+candle = Item("Candle", "Need some light?")
+jacket = Item("Jacket", "Its cold outside!")
+
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'outside':  
+        Room(
+            "Outside Cave Entrance", 
+            "North of you, the cave mount beckons", 
+            [cookies, mojito, pizza]
+        ),
+    'foyer':    
+        Room(
+            "Foyer", 
+            """Dim light filters in from the south. Dusty passages run north and east.""", 
+            [hacksaw, torch, shoes]
+        ),
+    'overlook': 
+        Room(
+            "Grand Overlook", 
+            """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.""", 
+            [pumpkin, knife, toilet]
+        ),
+    'narrow':   
+        Room(
+            "Narrow Passage", 
+            """The narrow passage bends here from west to north. The smell of gold permeates the air.""", 
+            [pillow, pen, paper]
+        ),
+    'treasure': 
+        Room(
+            "Treasure Chamber", 
+            """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.""", 
+            [pony, candle, jacket]
+        ),
 }
 
 
@@ -43,7 +73,14 @@ room['treasure'].s_to = room['narrow']
 player1 = Player("Anthony", room['outside'])
 # Write a loop that:
 #
+
+listed_item = [f"{i.name}: {i.description}" for i in player1.current_room.item_list]
+
+
 print(player1.current_room)
+for i in range(0, len(listed_item)):
+    print(listed_item[i])
+print(f"\n")
 user = input("[n] North  [s] South  [e] East  [w] West  [q] Quit\n")
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
@@ -53,19 +90,67 @@ user = input("[n] North  [s] South  [e] East  [w] West  [q] Quit\n")
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+
 while not user  == "q":
     if user == "n":
-        player1.current_room = player1.current_room.n_to
-        print(player1.current_room)
+        if hasattr(player1.current_room, 'n_to'):
+            player1.current_room = player1.current_room.n_to
+            listed_item = [f"{i.name}: {i.description}" for i in player1.current_room.item_list]
+            print(f"\n\n{player1.current_room}")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
+            print(f"\n")
+        else:
+            print(f"\n\n*******Invalid Direction Please Select Again\n{player1.current_room}")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
     elif user == "s":
-        player1.current_room = player1.current_room.s_to
-        print(player1.current_room)    
+        if hasattr(player1.current_room, 's_to'):
+            player1.current_room = player1.current_room.s_to
+            listed_item = [f"{i.name}: {i.description}" for i in player1.current_room.item_list]
+            print(f"\n\n{player1.current_room}")
+            print("Available Items in this Room:")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
+            print(f"\n")
+        else:
+            print(f"\n\n*******Invalid Direction Please Select Again\n{player1.current_room}") 
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
     elif user == "e":
-        player1.current_room = player1.current_room.e_to
-        print(player1.current_room)
+        if hasattr(player1.current_room, 'e_to'):
+            player1.current_room = player1.current_room.e_to
+            listed_item = [f"{i.name}: {i.description}" for i in player1.current_room.item_list]
+            print(f"\n\n{player1.current_room}")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
+            print(f"\n")
+        else:
+            print(f"\n\n*******Invalid Direction Please Select Again\n{player1.current_room}") 
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
     elif user == "w":
-        player1.current_room = player1.current_room.w_to
-        print(player1.current_room)
+        if hasattr(player1.current_room, 'w_to'):
+            player1.current_room = player1.current_room.w_to
+            listed_item = [f"{i.name}: {i.description}" for i in player1.current_room.item_list]
+            print(f"\n\n{player1.current_room}")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
+            print(f"\n")
+        else:
+            print(f"\n\n*******Invalid Direction Please Select Again\n{player1.current_room}")
+            print("Available Items in this Room:")
+            for i in range(0, len(listed_item)):
+                print(listed_item[i])
     else:
         print("Invalid Selection: Please try again")
 
--- src/room.py
+++ src/room.py
@@ -1,9 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_list):
         self.name = name
         self.description = description
+        self.item_list = item_list
     
     def __str__(self):
-        return f'Room: {self.name} \n Description: {self.description}'
+        return f'Room: {self.name} \n Description: {self.description} \n'
